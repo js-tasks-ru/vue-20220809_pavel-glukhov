39269d4744bfab0ad157692ada49a3a1bdd84436
Pavel Glukhov <pvglukhov@gmail.com>
2022-08-10 14:30:17 +0300

    [master] lesson 2. added MeetupList. ready for review

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,5 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import {createApp} from './vendor/vue.esm-browser.js';
+
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -15,3 +16,25 @@ function fetchMeetupById(meetupId) {
 }
 
 // Требуется создать Vue приложение
+createApp({
+  data() {
+    return {
+      meetup: 1,
+      meetupTitle: ``,
+    };
+  },
+  watch: {
+    meetup: {
+      handler(newValue, oldValue) {
+        try {
+          fetchMeetupById(newValue).then((meetup) => {
+            this.meetupTitle = meetup.title
+          });
+        } catch (err) {
+          console.error(`Error while requesting meetup ${err}`);
+        }
+      },
+      immediate: true
+    }
+  },
+}).mount('#app');
