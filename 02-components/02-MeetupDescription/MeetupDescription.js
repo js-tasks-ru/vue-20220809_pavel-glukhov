ef8b6024600f55f93abb9313c53900b5ce7d2fbe
Pavel Glukhov <pvglukhov@gmail.com>
2022-08-11 23:40:01 +0300

    [master] lessons -components. task - 2.

--- 02-components/02-MeetupDescription/MeetupDescription.js
+++ 02-components/02-MeetupDescription/MeetupDescription.js
@@ -2,6 +2,13 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupDescription',
+  // Компонент должен иметь входной параметр
+  props: {
+    description: {
+      default: ``,
+      type: String,
+    },
+  },
 
-  template: `<p class="meetup-description">Описание должно быть здесь</p>`,
+  template: `<p class="meetup-description">{{ description }}</p>`,
 });
