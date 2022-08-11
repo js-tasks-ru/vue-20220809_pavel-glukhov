4b50e4d4df9c421846fc039af7466b706bf2d894
Pavel Glukhov <pvglukhov@gmail.com>
2022-08-12 00:37:30 +0300

    [master] lessons - components. task - 3.

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -2,9 +2,21 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupCover',
-
+  props: {
+    title: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  },
+  computed: {
+    meetupImage() {
+      return this.image ? `url(${this.image})` : ``;
+    },
+  },
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <h1 class="meetup-cover__title">Title</h1>
+    <div class="meetup-cover"  :style="{ '--bg-url': meetupImage }">
+        <h1 class="meetup-cover__title">{{ title }}</h1>
     </div>`,
 });
