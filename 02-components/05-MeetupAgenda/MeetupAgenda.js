c19371d793755e7bbd946c7345ec99868005ac76
Pavel Glukhov <pvglukhov@gmail.com>
2022-08-17 14:44:51 +0300

    [master] lesson components. task - 5

--- 02-components/05-MeetupAgenda/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/MeetupAgenda.js
@@ -1,13 +1,23 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupAgenda',
+  components: {
+    MeetupAgendaItem,
+  },
+
+  props: {
+    agenda: {
+      type: Array,
+      required:true,
+    },
+  },
 
   template: `
     <ul class="agenda">
-      <li class="agenda__item">
-        <!-- meetup agenda item -->
+      <li class="agenda__item" v-for="meetupAgenda in agenda">
+        <meetup-agenda-item :agenda-item="meetupAgenda"></meetup-agenda-item>
       </li>
     </ul>`,
 });
