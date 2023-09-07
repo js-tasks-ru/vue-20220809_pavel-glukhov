463e3415806fccb0713306bc106745e696a477a4
Pavel Glukhov <pvglukhov@gmail.com>
2023-09-08 02:39:40 +0300

    [master] fixed some tests

--- 05-vue-router/05-AuthGuard/views/PageCreateMeetup.vue
+++ 05-vue-router/05-AuthGuard/views/PageCreateMeetup.vue
@@ -3,13 +3,9 @@
 </template>
 
 <script>
-import { login } from "../services/authService";
 
 export default {
-  name: 'PageCreateMeetup',
-  beforeRouteEnter(to, from) {
-    return login() ? true : '/login' ;
-  },
+  name: 'PageCreateMeetup'
 };
 </script>
 
